c21c8a27387069cb46587f09b3260f59f0657388
Joshua Williams <j.o-williams@outlook.com>
2020-01-31 01:36:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,12 +18,10 @@ def speak_to_grandma(phrase)
   elsif phrase.downcase
     return "HUH?!, SPEAK UP, SONNY!"
   else phrase.upcase
-    return "NO, NOT SINCE 1938!
-  
-  
+    return "NO, NOT SINCE 1938!"
   end
-
-
 end
+
+
 
 
